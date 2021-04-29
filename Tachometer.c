74cd642897294665cd867d9eb008c76496bf3b7d
Preston Brazzle <71470122+pbrazzle@users.noreply.github.com>
2021-04-29 12:20:21 -0400

    Add files via upload
    get_velocity functions return positive for forward and negative for backwards now

--- Tachometer.c
+++ Tachometer.c
@@ -6,13 +6,19 @@ uint16_t firstRightTime, secondRightTime;
 uint16_t firstLeftTime, secondLeftTime;
 uint16_t leftPeriod, rightPeriod;
 uint16_t leftDegrees = 0, rightDegrees = 0;
+uint16_t rightForward = 0, leftForward = 0;
 
 void Tachometer_Init(void){
   //right 10.4 left 5.2/10.5
+  //right dir 5.0 left dir 5.2
   //GPIO
   P10->SEL0 |= 0x30;
   P10->SEL1 &= ~0x30;
   P10->DIR &= ~0x30;
+  
+  P5->SEL0 &= ~0x05;
+  P5->SEL1 &= ~0x05;
+  P5->DIR &= ~0x05;
 
   //Halt TimerA3
   TIMER_A3->CTL &= ~0x0030;
@@ -37,6 +43,8 @@ void TA3_0_IRQHandler(void){
   //rightPeriod = secondRightTime - firstRightTime;
   rightPeriod = firstRightTime - secondRightTime;
   rightDegrees = (rightDegrees > 0) ? rightDegrees-1 : 0;
+  if (P5->IN & 0x01) rightForward = 1;
+  else rightForward = 0;
 }
 
 //Left tachometer interrupt
@@ -47,6 +55,8 @@ void TA3_N_IRQHandler(void){
   //leftPeriod = secondLeftTime - firstLeftTime;
   leftPeriod = firstLeftTime - secondLeftTime;
   leftDegrees = (leftDegrees > 0) ? leftDegrees-1 : 0;
+  if (P5->IN & 0x04) leftForward = 1;
+  else leftForward = 0;
 }
 
 void turnLeft(int degrees)
@@ -73,10 +83,14 @@ uint16_t getRightPeriod()
 
 double get_velocity_left()
 {
-    return (leftPeriod) * 0.083 * 0.001 * 18 / 11;
+	double vel = (leftPeriod) * 0.083 * 0.001 * 18 / 11;
+	if (!leftForward) vel = -vel;
+    return vel;
 }
 
 double get_velocity_right()
 {
-    return (rightPeriod) * 0.083 * 0.001 * 18 / 11;
+	double vel = (rightPeriod) * 0.083 * 0.001 * 18 / 11;
+	if (!rightForward) vel = -vel;
+    return vel;
 }
